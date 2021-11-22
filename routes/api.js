7d522615019914aa456e6bccf4fa082d305bc251
SilverSSSC <CFrank306@gmail.com>
2021-11-21 23:26:07 -0600

    finished first draft of api routes

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,42 @@ const router = require("express").Router();
 const Workout = require("../models/workout");
 
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
     .then(dbWorkout => {
-        res.json(dbWorkout);
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({body}, res) => {
+    db.workouts.update({}, {set: {body}})
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
+router.get("/api/workouts", (req, res) => {
+    Workouts.find({})
+        .sort({ date: -1 })
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", ({body},res) => {
+    Workouts.insertMany(body)
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
     })
     .catch(err => {
         res.status(400).json(err);
